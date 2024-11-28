2b3afd9ecae5d560d41e95b967ba547b8f0b36b8
Arjun Narzary <arjunn@byteridge.com>
2024-11-28 21:36:09 +0530

    Added expect to check for multiple delimiter with length greater than 1

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -51,5 +51,6 @@ describe("String Calculator", () => {
 
   test("Should return sum with multiple delimiter with any length", () => {
     expect(calculator.add("//[*][%]\n1*2%3")).toBe(6)
+    expect(calculator.add("//[**][%%%]\n1**2%%%3")).toBe(6)
   })
 })
