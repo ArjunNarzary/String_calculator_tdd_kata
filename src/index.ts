546c7663a1026d431008fc6b65e4b378637e74e4
Arjun Narzary <arjunn@byteridge.com>
2024-11-28 20:13:16 +0530

    refector code to support custom delimitter

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
 class StringCalculator {
   public add(text: string): number | string | undefined {
     if (!text) return 0
-    console.log(this.splitText(text))
     const allCharacters: string[] = this.splitText(text)
     const arrayOfNumber = this.convertToNumber(allCharacters)
     return arrayOfNumber.reduce((a, b) => a + Number(b), 0)
   }
 
   private splitText(text: string): string[] {
-    if (text.includes("//")) {
-      //Separate delimitter to new variable as delimiter
-      const [delimiter, ...rest] = text.split("\n")
-      //Now extract delimitter from \\; by slicing
-      const customDelimitter = delimiter.slice(2)
-
-      return rest.join("").split(customDelimitter)
+    if (this.hasCustomDelimitter(text)) {
+      return this.splitTextForCustomDelimitter(text)
     }
     return text.split(/[,|\n]/)
   }
 
   private convertToNumber(arryOfString: string[]): number[] {
     return arryOfString.map((item) => Number(item))
+  }
+
+  private hasCustomDelimitter(text: string): boolean {
+    return text.includes("//")
+  }
+
+  private splitTextForCustomDelimitter(text: string): string[] {
+    //Separate delimitter to new variable as delimiter
+    const [delimiter, ...rest] = text.split("\n")
+    //Now extract delimitter from \\; by slicing
+    const customDelimitter = delimiter.slice(2)
+
+    return rest.join("").split(customDelimitter)
   }
 }
 
