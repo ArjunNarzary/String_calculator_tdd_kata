112bbf38368d5b4887ce2a5268fd3ddda23fc605
Arjun Narzary <arjunn@byteridge.com>
2024-11-28 19:24:46 +0530

    Refectored code to pass adding numbers delimitted by comma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 class StringCalculator {
   public add(text: string): number | string | undefined {
-    if (text) {
-      return parseInt(text)
-    } else {
-      return 0
+    if (!text) return 0
+    if (text.includes(",")) {
+      const numbers = text.split(",")
+      return numbers.reduce((a, b) => a + Number(b), 0)
     }
+    return Number(text)
   }
 }
 
